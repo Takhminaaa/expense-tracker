11eb8cbc38687863ce2cc2480fdf1a96ef49a368
Тахмина Калмамат кызы <tahmina1412@icloud.com>
2024-07-02 21:54:35 +0300

    Finished login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-import  { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewExpense } from "./components/new-expense/NewExpense";
 import { EXPENSES } from "./utils/constants";
 import { Expenses } from "./components/expenses/Expenses";
-import { Chart } from "./components/chart/Chart";
-
+import Header from "./components/header/HEader";
+import Login from "./components/login/Login";
 
 const App = () => {
   const [expenses, setExpenses] = useState(EXPENSES);
- 
+  const [isLogin, setIsLogin] = useState(false);
+
   console.log(expenses);
 
   const onAddExpense = (newExpense) => {
     setExpenses([...expenses, newExpense]);
   };
 
+  const loginHandler = () => {
+    setIsLogin(true);
 
- 
+    localStorage.setItem("auth", !isLogin);
+  };
+
+  useEffect(() => {
+    const isAuth = localStorage.getItem("auth");
+    setIsLogin(isAuth);
+  }, []);
+  const logOutHandler = () => {
+    setIsLogin(false);
+    localStorage.removeItem("auth");
+  };
 
   return (
-    <div>
-      <NewExpense onAddExpense={onAddExpense} />
-     
-      <Expenses expenses={expenses}/>
-      
-    </div>
+    <>
+      <Header isLogin={isLogin} onLogout={logOutHandler} />
+
+      {isLogin ? (
+        <>
+          <NewExpense onAddExpense={onAddExpense} />
+
+          <Expenses expenses={expenses} />
+        </>
+      ) : (
+        <Login onLogin={loginHandler} />
+      )}
+    </>
   );
 };
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../UI/button/Button";
+
+const Header = ({ isLogin, onLogout }) => {
+  return (
+    <StyledHeader>
+      <h2>Expense Tracker</h2>
+
+      {isLogin && (
+        <ContainerButton>
+          <Button>Expense</Button>
+          <Button>Users</Button>
+          <Button onClick={onLogout}>Logaut</Button>
+        </ContainerButton>
+      )}
+    </StyledHeader>
+  );
+};
+
+export default Header;
+const ContainerButton = styled("div")`
+  display: flex;
+  justify-content: center;
+  justify-content: end;
+  align-items: center;
+  gap: 20px;
+`;
+
+const StyledHeader = styled.header`
+  width: 100%;
+  height: 100px;
+  background: #ad9be9;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px 0 40px;
+  align-items: center;
+
+  h2 {
+    font-size: 22px;
+    font-weight: bold;
+    color: white;
+  }
+`;
